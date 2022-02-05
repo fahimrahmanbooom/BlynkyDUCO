3af0f39ea464bbb11a1b3b6395be0e3e411990ac
Fahim Rahman <fahimrahman@MacBookPro.local>
2022-02-06 05:42:46 +0600

    Initial

---
+++ BlynkyDUCO_ESP8266/BlynkyDUCO_ESP8266.ino
@@ -0,0 +1,485 @@
+/*
+  __BLYNKY DUCO__
+  ---------------
+  BLYNKY DUCO is a Blynk client and a Miner at the same time. 
+  While Blynk is idle, why not earn some Cryptocurrency!
+
+  Modified from official Duino-Coin ESP8266 Code,
+  https://github.com/revoxhere/duino-coin and
+  BLYNK 2.0 Code, https://blynk.io/
+
+  DUCO version 3.0
+  BLYNK version 0.1.0
+  BLYNK API version 2.0
+
+  Author: Fahim Rahman, https://github.com/fahimrahmanbooom
+
+*/
+
+
+/*
+
+  Try this configuration for better performance before flashing.
+
+  Board: "Select your ESP8266 Board"
+  Upload Speed: "115200"
+  CPU Frequency: "160 MHz"
+  Flash Size: "4MB (FS:3MB OTA:~-512KB)"
+  Flash Mode: "DOUT"
+  Flash Frequency: "26MHz"
+  IwIP Variant: "V2 Higher Bandwidth"
+  Stack Protection: "Enabled"
+  MMU: "32KB cache + 32KB IRAM (balanced)"
+
+*/
+
+
+/*
+  HOW TO SETUP:
+
+  Step 1:
+  ------
+  Go to https://blynk.cloud/dashboard then Device Info and copy;
+  BLYNK_TEMPLATE_ID, BLYNK_DEVICE_NAME, BLYNK_AUTH_TOKEN then paste it below.
+
+  Step 1:
+  ------
+  Fill up your Wifi SSID, Password, Duino-Coin username and RIG Identifier below.
+
+  Step 3:
+  ------
+  Flash using the recommended configuration.
+
+  Step 4:
+  ------
+  Relax and Don't forget to leave a star. Thanks :)
+
+*/
+
+
+// Fill-in information from your Blynk Template here !!!! !!!! !!!!
+
+#define BLYNK_TEMPLATE_ID   "**********"
+#define BLYNK_DEVICE_NAME   "*********"
+#define BLYNK_AUTH_TOKEN    "***-*******-*****_*****"
+
+#define BLYNK_FIRMWARE_VERSION "0.1.0"
+#define BLYNK_PRINT Serial
+
+#pragma GCC optimize ("-Ofast")
+
+#include <BlynkSimpleEsp8266.h>
+#include <ArduinoJson.h>
+#include <bearssl/bearssl.h>
+#include <TypeConversion.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Ticker.h>
+
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+
+// Your Own Blynk Logic Here -
+// Here is an example for two channel relay attached to a Wemos D1 Mini Board.
+// But you can use any ESP8266 Board.
+
+// define your virtual pin configured on blynk dashboard.
+BLYNK_WRITE(V0) {
+  // get the pin value from the server.
+  int pinValue = param.asInt();
+  // writing the pin value on the board.
+  digitalWrite(D5, pinValue);
+}
+
+BLYNK_WRITE(V1) {
+  int pinValue = param.asInt();
+  digitalWrite(D7, pinValue);
+}
+
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+
+namespace {
+
+// Change the part in brackets to your WiFi name
+const char* SSID = "*************";
+// Change the part in brackets to your WiFi password
+const char* PASSWORD = "*************";
+// Change the part in brackets to your Duino-Coin username
+const char* USERNAME = "************";
+// Change the part in brackets if you want to set a custom miner name (use Auto to autogenerate)
+const char* RIG_IDENTIFIER = "************";
+// Change false to true if using 160 MHz clock mode to not get the first share rejected
+const bool USE_HIGHER_DIFF = true;
+// Change true to false if you want to disable led blinking(But the LED will work in the beginning until esp connects to the pool)
+const bool LED_BLINKING = true;
+
+
+
+/* Do not change the lines below. These lines are static and dynamic variables
+   that will be used by the program for counters and measurements. */
+const char * DEVICE = "ESP8266";
+const char * POOLPICKER_URL[] = {"https://server.duinocoin.com/getPool"};
+const char * MINER_BANNER = "Official ESP8266 Miner";
+const char * MINER_VER = "3.0";
+unsigned int share_count = 0;
+unsigned int port = 0;
+unsigned int difficulty = 0;
+float hashrate = 0;
+String AutoRigName = "";
+String host = "";
+String node_id = "";
+
+
+void UpdateHostPort(String input) {
+  // Thanks @ricaun for the code
+  DynamicJsonDocument doc(256);
+  deserializeJson(doc, input);
+  const char* name = doc["name"];
+
+  host = String((const char*)doc["ip"]);
+  port = int(doc["port"]);
+  node_id = String(name);
+
+  Serial.println("Poolpicker selected the best mining node: " + node_id);
+}
+
+String httpGetString(String URL) {
+  String payload = "";
+  WiFiClientSecure client;
+  client.setInsecure();
+  HTTPClient http;
+
+  if (http.begin(client, URL)) {
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK) payload = http.getString();
+    else Serial.printf("Error fetching node from poolpicker: %s\n", http.errorToString(httpCode).c_str());
+
+    http.end();
+  }
+  return payload;
+}
+
+void UpdatePool() {
+  String input = "";
+  int waitTime = 1;
+  int poolIndex = 0;
+  int poolSize = sizeof(POOLPICKER_URL) / sizeof(char*);
+
+  while (input == "") {
+    Serial.println("Fetching mining node from the poolpicker in " + String(waitTime) + "s");
+    input = httpGetString(POOLPICKER_URL[poolIndex]);
+    poolIndex += 1;
+
+    // Check if pool index needs to roll over
+    if ( poolIndex >= poolSize ) {
+      poolIndex %= poolSize;
+      delay(waitTime * 1000);
+
+      // Increase wait time till a maximum of 32 seconds (addresses: Limit connection requests on failure in ESP boards #1041)
+      waitTime *= 2;
+      if ( waitTime > 32 )
+        waitTime = 32;
+    }
+  }
+
+  // Setup pool with new input
+  UpdateHostPort(input);
+}
+
+WiFiClient client;
+String client_buffer = "";
+String chipID = "";
+String START_DIFF = "";
+
+// Loop WDT... please don't feed me...
+// See lwdtcb() and lwdtFeed() below
+Ticker lwdTimer;
+#define LWD_TIMEOUT   60000
+
+unsigned long lwdCurrentMillis = 0;
+unsigned long lwdTimeOutMillis = LWD_TIMEOUT;
+
+#define END_TOKEN  '\n'
+#define SEP_TOKEN  ','
+
+#define LED_BUILTIN 2
+
+#define BLINK_SHARE_FOUND    1
+#define BLINK_SETUP_COMPLETE 2
+#define BLINK_CLIENT_CONNECT 3
+#define BLINK_RESET_DEVICE   5
+
+void SetupWifi() {
+  Serial.println("Connecting to: " + String(SSID));
+  WiFi.mode(WIFI_STA); // Setup ESP in client mode
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+  WiFi.begin(SSID, PASSWORD);
+
+  int wait_passes = 0;
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (++wait_passes >= 10) {
+      WiFi.begin(SSID, PASSWORD);
+      wait_passes = 0;
+    }
+  }
+
+  Serial.println("\n\nnSuccessfully connected to WiFi");
+  Serial.println("Local IP address: " + WiFi.localIP().toString());
+  Serial.println("Rig name: " + String(RIG_IDENTIFIER));
+  Serial.println();
+
+  UpdatePool();
+}
+
+void SetupOTA() {
+  // Prepare OTA handler
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.setHostname(RIG_IDENTIFIER); // Give port a name not just address
+  ArduinoOTA.begin();
+}
+
+void blink(uint8_t count, uint8_t pin = LED_BUILTIN) {
+  if (LED_BLINKING) {
+    uint8_t state = HIGH;
+
+    for (int x = 0; x < (count << 1); ++x) {
+      digitalWrite(pin, state ^= HIGH);
+      delay(50);
+    }
+  }
+}
+
+void RestartESP(String msg) {
+  Serial.println(msg);
+  Serial.println("Restarting ESP...");
+  blink(BLINK_RESET_DEVICE);
+  ESP.reset();
+}
+
+// Our new WDT to help prevent freezes
+// code concept taken from https://sigmdel.ca/michel/program/esp8266/arduino/watchdogs2_en.html
+void ICACHE_RAM_ATTR lwdtcb(void) {
+  if ((millis() - lwdCurrentMillis > LWD_TIMEOUT) || (lwdTimeOutMillis - lwdCurrentMillis != LWD_TIMEOUT))
+    RestartESP("Loop WDT Failed!");
+}
+
+void lwdtFeed(void) {
+  lwdCurrentMillis = millis();
+  lwdTimeOutMillis = lwdCurrentMillis + LWD_TIMEOUT;
+}
+
+void VerifyWifi() {
+  while (WiFi.status() != WL_CONNECTED || WiFi.localIP() == IPAddress(0, 0, 0, 0))
+    WiFi.reconnect();
+}
+
+void handleSystemEvents(void) {
+  VerifyWifi();
+  ArduinoOTA.handle();
+  yield();
+}
+
+// https://stackoverflow.com/questions/9072320/split-string-into-string-array
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int max_index = data.length() - 1;
+
+  for (int i = 0; i <= max_index && found <= index; i++) {
+    if (data.charAt(i) == separator || i == max_index) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == max_index) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void waitForClientData(void) {
+  client_buffer = "";
+
+  while (client.connected()) {
+    if (client.available()) {
+      client_buffer = client.readStringUntil(END_TOKEN);
+      if (client_buffer.length() == 1 && client_buffer[0] == END_TOKEN)
+        client_buffer = "???\n"; // NOTE: Should never happen
+
+      break;
+    }
+    handleSystemEvents();
+  }
+}
+
+void ConnectToServer() {
+  if (client.connected())
+    return;
+
+  Serial.println("\n\nConnecting to the Duino-Coin server...");
+  while (!client.connect(host, port));
+
+  waitForClientData();
+  Serial.println("Connected to the server. Server version: " + client_buffer );
+  blink(BLINK_CLIENT_CONNECT); // Successfull connection with the server
+}
+
+bool max_micros_elapsed(unsigned long current, unsigned long max_elapsed) {
+  static unsigned long _start = 0;
+
+  if ((current - _start) > max_elapsed) {
+    _start = current;
+    return true;
+  }
+  return false;
+}
+} // namespace
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // set pinMode for blynk.
+  pinMode(D5, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.println("\nDuino-Coin " + String(MINER_VER));
+
+  // Autogenerate ID if required
+  chipID = String(ESP.getChipId(), HEX);
+  if ( strcmp(RIG_IDENTIFIER, "Auto") == 0 ) {
+    AutoRigName = "ESP8266-" + chipID;
+    AutoRigName.toUpperCase();
+    RIG_IDENTIFIER = AutoRigName.c_str();
+  }
+
+  SetupWifi();
+  SetupOTA();
+
+  lwdtFeed();
+  lwdTimer.attach_ms(LWD_TIMEOUT, lwdtcb);
+  if (USE_HIGHER_DIFF) START_DIFF = "ESP8266H";
+  else START_DIFF = "ESP8266";
+
+  blink(BLINK_SETUP_COMPLETE);
+
+  // run blynk.
+  Blynk.config(BLYNK_AUTH_TOKEN);
+}
+
+void loop()
+{
+  // run blynk continuously.
+  Blynk.run();
+
+  // run duco miner continuoisly.
+  br_sha1_context sha1_ctx, sha1_ctx_base;
+  uint8_t hashArray[20];
+  String duco_numeric_result_str;
+
+  // 1 minute watchdog
+  lwdtFeed();
+
+  // OTA handlers
+  VerifyWifi();
+  ArduinoOTA.handle();
+
+  ConnectToServer();
+  Serial.println("Asking for a new job for user: " + String(USERNAME));
+  client.print("JOB," + String(USERNAME) + "," + String(START_DIFF));
+
+  waitForClientData();
+  String last_block_hash = getValue(client_buffer, SEP_TOKEN, 0);
+  String expected_hash = getValue(client_buffer, SEP_TOKEN, 1);
+  difficulty = getValue(client_buffer, SEP_TOKEN, 2).toInt() * 100 + 1;
+
+  int job_len = last_block_hash.length() + expected_hash.length() + String(difficulty).length();
+  Serial.println("Received a correct job with size of " + String(job_len) + " bytes");
+  expected_hash.toUpperCase();
+  br_sha1_init(&sha1_ctx_base);
+  br_sha1_update(&sha1_ctx_base, last_block_hash.c_str(), last_block_hash.length());
+
+  float start_time = micros();
+  max_micros_elapsed(start_time, 0);
+
+  String result = "";
+  digitalWrite(LED_BUILTIN, HIGH);
+  for (unsigned int duco_numeric_result = 0; duco_numeric_result < difficulty; duco_numeric_result++) {
+    // Difficulty loop
+    sha1_ctx = sha1_ctx_base;
+    duco_numeric_result_str = String(duco_numeric_result);
+    br_sha1_update(&sha1_ctx, duco_numeric_result_str.c_str(), duco_numeric_result_str.length());
+    br_sha1_out(&sha1_ctx, hashArray);
+    result = experimental::TypeConversion::uint8ArrayToHexString(hashArray, 20);
+    if (result == expected_hash) {
+      // If result is found
+      unsigned long elapsed_time = micros() - start_time;
+      float elapsed_time_s = elapsed_time * .000001f;
+      hashrate = duco_numeric_result / elapsed_time_s;
+      share_count++;
+      blink(BLINK_SHARE_FOUND);
+
+      client.print(String(duco_numeric_result)
+                   + ","
+                   + String(hashrate)
+                   + ","
+                   + String(MINER_BANNER)
+                   + " "
+                   + String(MINER_VER)
+                   + ","
+                   + String(RIG_IDENTIFIER)
+                   + ",DUCOID"
+                   + String(chipID));
+
+      waitForClientData();
+      Serial.println(client_buffer
+                     + " share #"
+                     + String(share_count)
+                     + " (" + String(duco_numeric_result) + ")"
+                     + " hashrate: "
+                     + String(hashrate / 1000, 2)
+                     + " kH/s ("
+                     + String(elapsed_time_s)
+                     + "s)");
+      break;
+    }
+    if (max_micros_elapsed(micros(), 500000))
+    {
+      handleSystemEvents();
+    }
+    else
+    {
+      delay(0);
+    }
+  }
+}
